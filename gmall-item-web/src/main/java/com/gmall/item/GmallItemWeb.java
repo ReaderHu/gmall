2b591a5d1c57ef4d87f615c2e56d7768613e83bb
wangh <592593695@qq.com>
2020-07-19 17:10:07 +0800

    提交

--- gmall-item-web/src/main/java/com/gmall/item/GmallItemWeb.java
+++ gmall-item-web/src/main/java/com/gmall/item/GmallItemWeb.java
@@ -1,13 +1,14 @@
-package com.gmall.demo;
+package com.gmall.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GmallItemWeb {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(GmallItemWeb.class, args);
     }
 
 }
