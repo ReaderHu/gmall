ad7143ab13e2a0d9baa98fbee86c69b189845f68
ReaderHu <37507486+ReaderHu@users.noreply.github.com>
2022-07-18 21:59:06 +0800

    Update GmallUserApp.java

--- gmall-user/src/main/java/com/gmall/user/GmallUserApp.java
+++ gmall-user/src/main/java/com/gmall/user/GmallUserApp.java
@@ -10,7 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  *
  * @Description
  * @auther wang
- * @create 2020-06-28
+ * @create 2020-06-30
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.gmall.user.mapper")
